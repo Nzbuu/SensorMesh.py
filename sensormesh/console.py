62a2cc7a4629638f3e64aee44197f03a1b1fef20
James Myatt <james@jamesmyatt.co.uk>
2016-01-25 22:22:12 +0000

    Handle missing timestamp in console logger

--- sensormesh/console.py
+++ sensormesh/console.py
@@ -7,4 +7,10 @@ class ConsoleLogger(Logger):
         super().__init__()
 
     def update(self, timestamp=None, **kwargs):
-        print(datetime.fromtimestamp(timestamp).isoformat(), ':', kwargs)
+        if timestamp is None:
+            str_time = 'None'
+        else:
+            ts = datetime.fromtimestamp(timestamp)
+            str_time = ts.isoformat()
+
+        print(str_time, ':', kwargs)
