45bd6c8c10392a3d7515ebc02a9f2b6635f69c76
James Myatt <james@jamesmyatt.co.uk>
2016-02-02 22:45:06 +0000

    Change status to pre-alpha

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     license="MIT",
     url='https://github.com/Nzbuu/SensorMesh.py',
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
