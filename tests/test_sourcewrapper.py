229a9358fff69f946a2d53eb93d12311163aa7c4
James Myatt <james@jamesmyatt.co.uk>
2016-02-02 23:55:27 +0000

    (SourceWrapper) Check for exception when unmatched arguments are provided

--- tests/test_sourcewrapper.py
+++ tests/test_sourcewrapper.py
@@ -1,5 +1,7 @@
 from unittest.mock import Mock
 
+import pytest
+
 from sensormesh.base import DataSourceWrapper
 
 
@@ -7,6 +9,10 @@ class TestDataSourceWrapper:
     def test_can_create_without_callable(self):
         s = DataSourceWrapper(name='test_source')
         assert s.name == 'test_source'
+
+    def test_throws_on_extra_positional_args(self):
+        with pytest.raises(ValueError):
+            s = DataSourceWrapper('test_source', 'x')  # 'test_source' is name
 
     def test_can_wrap_one_callable(self):
         m = Mock(return_value=10)
