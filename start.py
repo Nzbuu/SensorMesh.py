f3820bb9171704cddd7c8fe088255aeae7c95367
James Myatt <james@jamesmyatt.co.uk>
2016-02-02 22:12:36 +0000

    (App) Update start script

--- start.py
+++ start.py
@@ -5,26 +5,26 @@ from sensormesh.base import DataSourceWrapper
 from sensormesh.console import ConsoleDisplay
 from sensormesh.thingspeak import ThingSpeakLogger
 
+# Configure App
+app = App()
+app.set_steps(step=20, num_steps=5)
 
-if __name__ == '__main__':
-    app = App()
+# Source
+s = DataSourceWrapper(value=random.random)
+app.add_source(s)
 
-    # Source
-    s = DataSourceWrapper(value=random.random)
-    app.add_source(s)
+# Target 1
+t = ConsoleDisplay()
+app.add_target(t)
 
-    # Target 1
-    t = ConsoleDisplay()
-    app.add_target(t)
+# Target 2
+t = ThingSpeakLogger.from_file('thingspeak.json')
+app.add_target(t)
 
-    # Target 2
-    t = ThingSpeakLogger.from_file('thingspeak.json')
-    app.add_target(t)
-
-    # Start application
-    try:
-        app.start()
-    except KeyboardInterrupt:
-        print("Goodbye!")
-    finally:
-        print("Stop!")
+# Start application
+try:
+    app.start()
+except KeyboardInterrupt:
+    print("Goodbye!")
+finally:
+    print("Stop!")
