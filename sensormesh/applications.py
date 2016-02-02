fced8568eccebc271b52c5a68a017686fa937577
James Myatt <james@jamesmyatt.co.uk>
2016-02-02 19:46:04 +0000

    (App) Make time and delay functions configurable

--- sensormesh/applications.py
+++ sensormesh/applications.py
@@ -4,12 +4,15 @@ from .exceptions import ConfigurationError
 
 
 class App(object):
-    def __init__(self):
-        self.name = "SensorMesh"
+    def __init__(self, name="SensorMesh", timefcn=None, delayfcn=None):
+        self.name = name
         self._source = None
         self._loggers = []
         self._step = 20
         self._num_steps = 5
+
+        self._timefcn = timefcn if timefcn else time.time
+        self._delayfcn = delayfcn if delayfcn else time.sleep
 
     def add_source(self, source):
         if self._source is None:
@@ -32,16 +35,17 @@ class App(object):
         self._check_for_source()
         self._check_for_loggers()
 
-        time_start_next = time.time()
+        time_start_next = self._timefcn()
         for count_steps in range(self._num_steps):
             self.step()
 
-            time_finish_now = time.time()
+            time_finish_now = self._timefcn()
             time_start_next += self._step
-            time.sleep(time_start_next - time_finish_now)
+            if count_steps < self._num_steps - 1:
+                self._delayfcn(time_start_next - time_finish_now)
 
     def step(self):
-        timestamp = time.time()
+        timestamp = self._timefcn()
 
         data = self._source.read()
         if not data.get('timestamp', None):
