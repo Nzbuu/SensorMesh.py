b25280ba21017c917e4837a1395d90a2b72e3c82
James Myatt <james@jamesmyatt.co.uk>
2016-02-02 23:17:36 +0000

    (Config) Check reading empty json too

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,9 +5,17 @@ import builtins
 from sensormesh.applications import load_config_file
 
 
-def test_can_read_config_file():
-    test_data = {'name': 'test_thing', 'key': 'ABCDEFGHIJ', 'feed': {'field1': 'A'}}
-    with patch.object(builtins, 'open', mock_open(read_data=json.dumps(test_data))):
-        cfg_data = load_config_file('config.json')
+class TestConfig:
+    def test_can_read_empty_file(self):
+        test_data = {}
+        with patch.object(builtins, 'open', mock_open(read_data=json.dumps(test_data))):
+            cfg_data = load_config_file('config.json')
 
-    assert cfg_data == test_data
+        assert cfg_data == test_data
+
+    def test_can_read_config_file(self):
+        test_data = {'name': 'test_thing', 'key': 'ABCDEFGHIJ', 'feed': {'field1': 'A'}}
+        with patch.object(builtins, 'open', mock_open(read_data=json.dumps(test_data))):
+            cfg_data = load_config_file('config.json')
+
+        assert cfg_data == test_data
