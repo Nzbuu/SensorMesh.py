427e1c61210870131d419cce126836594d2c5de7
James Myatt <james@jamesmyatt.co.uk>
2016-02-02 21:56:23 +0000

    (App) Use concrete in tests so that properties are exercised

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,37 +5,37 @@ from sensormesh.applications import *
 from sensormesh.base import DataSource, DataTarget
 
 
-class TestApp():
+class TestApp:
     def test_can_create_app(self):
         a = App()
 
     def test_can_add_source(self):
         a = App()
-        s = Mock(spec=DataSource)
+        s = DataSource()
         s.name = 'Mock Source'
         a.add_source(s)
         assert a.get_source_name() == 'Mock Source'
 
     def test_cannot_add_2_sources(self):
         a = App()
-        s1 = Mock(spec=DataSource)
-        s2 = Mock(spec=DataSource)
+        s1 = DataSource()
+        s2 = DataSource()
         a.add_source(s1)
         with pytest.raises(ConfigurationError):
             a.add_source(s2)
 
     def test_can_add_target(self):
         a = App()
-        t = Mock(spec=DataTarget)
+        t = DataTarget()
         t.name = 'Mock Target'
         a.add_target(t)
         assert a.get_target_names() == ['Mock Target']
 
     def test_can_add_2_targets(self):
         a = App()
-        t1 = Mock(spec=DataTarget)
+        t1 = DataTarget()
         t1.name = 'Mock Target 1'
-        t2 = Mock(spec=DataTarget)
+        t2 = DataTarget()
         t2.name = 'Mock Target 2'
         a.add_target(t1)
         a.add_target(t2)
