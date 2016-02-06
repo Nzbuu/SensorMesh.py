54c7e419f71ce1bee2947aa792aa891a351e9dae
James Myatt <james@jamesmyatt.co.uk>
2016-02-06 19:30:00 +0000

    (Rest) Add tests on mapping of fields

--- sensormesh/rest.py
+++ sensormesh/rest.py
@@ -5,12 +5,13 @@ from .exceptions import ConfigurationError
 class RestTarget(DataTarget):
     def __init__(self, name='', feeds=None, fields=None, api=None):
         super().__init__(name=name, feeds=feeds, fields=fields)
-        self._api = api
 
-    def update(self, data):
-        if not self._api:
+        if api:
+            self._api = api
+        else:
             raise ConfigurationError()
 
+    def update(self, data):
         content = self._prepare_update(data)
         self._api.post_update(content)
 
--- tests/test_rest.py
+++ tests/test_rest.py
@@ -6,23 +6,36 @@ from sensormesh.rest import *
 
 
 class TestRestTarget:
-    def test_can_create_target_without_api(self):
-        o = RestTarget(name='REST server')
-        assert o.name == 'REST server'
-        assert o._api is None
+    def test_cannot_create_target_without_api(self):
+        with pytest.raises(ConfigurationError):
+            o = RestTarget()
 
     def test_can_create_with_api(self):
         mock_api = mock.Mock()
-        o = RestTarget(api=mock_api)
+        o = RestTarget(api=mock_api, name='REST server')
         assert o._api is mock_api
+        assert o.name == 'REST server'
 
-    def test_cannot_update_without_api(self):
-        o = RestTarget()
-        with pytest.raises(ConfigurationError):
-            o.update({'value': 1})
+    def test_can_update_with_feeds(self):
+        mock_api = mock.Mock()
+        o = RestTarget(api=mock_api, feeds={'field1': 'value'})
+        o.update({'value': 1})
+        mock_api.post_update.assert_called_with({'field1': 1})
 
-    def test_can_update_with_api(self):
+    def test_can_update_without_feeds(self):
         mock_api = mock.Mock()
         o = RestTarget(api=mock_api)
         o.update({'value': 1})
         mock_api.post_update.assert_called_with({'value': 1})
+
+    def test_can_update_with_missing_inputs(self):
+        mock_api = mock.Mock()
+        o = RestTarget(api=mock_api, feeds={'field1': 'value', 'field2': 'count'})
+        o.update({'value': 1})
+        mock_api.post_update.assert_called_with({'field1': 1})
+
+    def test_can_update_with_extra_inputs(self):
+        mock_api = mock.Mock()
+        o = RestTarget(api=mock_api, feeds={'field1': 'value'})
+        o.update({'value': 1, 'other': 0})
+        mock_api.post_update.assert_called_with({'field1': 1})
