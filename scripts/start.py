1abd105d5ed288d01dfa71e46cd546fceb0bc266
James Myatt <james@jamesmyatt.co.uk>
2016-02-09 00:28:07 +0000

    (Scripts) Change working directory for scripts

--- scripts/start.py
+++ scripts/start.py
@@ -22,12 +22,12 @@ t = ConsoleDisplay()
 app.add_target(t)
 
 # Target 2
-tsl_config = cfg_man.load_config_file('scripts/thingspeak.json')
+tsl_config = cfg_man.load_config_file('thingspeak.json')
 t = ThingSpeakLogger(**tsl_config)
 app.add_target(t)
 
 # Target 3
-t = TextLogger(filename='scripts/testdata.csv', fields=['timestamp', 'value'])
+t = TextLogger(filename='testdata.csv', fields=['timestamp', 'value'])
 app.add_target(t)
 
 # Start application
