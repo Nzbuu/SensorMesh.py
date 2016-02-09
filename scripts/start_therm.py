49071bd2945e22c48b28fb14c0fc6953671210a3
James Myatt <james@jamesmyatt.co.uk>
2016-02-09 13:28:15 +0000

    (scripts) Add example logging from w1therm

---
+++ scripts/start_therm.py
@@ -0,0 +1,43 @@
+import random
+
+from w1thermsensor import W1ThermSensor
+
+from sensormesh.application import Controller, ConfigManager
+from sensormesh.base import DataSourceWrapper
+from sensormesh.console import ConsoleDisplay
+from sensormesh.thingspeak import ThingSpeakLogger
+from sensormesh.text import TextLogger
+
+# Configuration loader class
+cfg_man = ConfigManager()
+
+# Configure App
+app = Controller()
+app.set_steps(step=60, num_steps=24*60)
+
+# Source
+o = W1ThermSensor()
+print(o)
+s = DataSourceWrapper(fields=['temperature'], source=o.get_temperature)
+app.add_source(s)
+
+# Target 1
+t = ConsoleDisplay()
+app.add_target(t)
+
+# Target 2
+tsl_config = cfg_man.load_config_file('thingspeak_therm.json')
+t = ThingSpeakLogger(**tsl_config)
+app.add_target(t)
+
+# Target 3
+t = TextLogger(filename='logdata_therm.csv', fields=['timestamp', 'temperature'])
+app.add_target(t)
+
+# Start application
+try:
+    app.start()
+except KeyboardInterrupt:
+    print("Goodbye!")
+finally:
+    print("Stop!")
