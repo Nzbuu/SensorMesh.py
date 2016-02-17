11a31f3ce41f4cc55e285ca863ef09bf76b2fe1b
James Myatt <james@jamesmyatt.co.uk>
2016-02-17 22:53:01 +0000

    (Setup) Update version to 0.0.3.dev1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SensorMesh",
-    version="0.0.2.dev2",
+    version="0.0.3.dev1",
     packages=find_packages(),
 
     install_requires=['requests', 'python-dateutil'],
