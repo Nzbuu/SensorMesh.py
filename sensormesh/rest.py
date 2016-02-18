6fe46c3ea78faf9d82320df09f22cab980fd8150
James Myatt <james@jamesmyatt.co.uk>
2016-02-18 18:25:37 +0000

    (REST) Further simplification of creation of API object

--- sensormesh/rest.py
+++ sensormesh/rest.py
@@ -1,37 +1,29 @@
-import inspect
-
 from .endpoints import DataSource, DataTarget
 
 
 class RestApi(object):
-    pass
+    @classmethod
+    def create_api(cls, api):
+        if isinstance(api, dict):
+            api = cls(**api)
+        elif not api:
+            raise ValueError('Missing API input.')
+        return api
 
 
 class RestTarget(DataTarget):
-    def __init__(self, api=None, api_cls=None, *args, **kwargs):
+    def __init__(self, api=None, api_cls=RestApi, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        if isinstance(api, dict):
-            self._api = api_cls(**api)
-        elif api:
-            self._api = api
-        else:
-            raise ValueError('Missing API input.')
+        self._api = api_cls.create_api(api)
 
     def _update(self, data):
         self._api.post_update(data)
 
 
 class RestSource(DataSource):
-    def __init__(self, api=None, api_cls=None, *args, **kwargs):
+    def __init__(self, api=None, api_cls=RestApi, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        if isinstance(api, dict):
-            self._api = api_cls(**api)
-        elif api:
-            self._api = api
-        else:
-            raise ValueError('Missing API input.')
+        self._api = api_cls.create_api(api)
 
     def _read(self):
         return self._api.get_data()
